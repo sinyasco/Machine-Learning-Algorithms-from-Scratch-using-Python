aba083956a8bb113227be126d87105a688a91fdf
yacineziainia <ny_ziainia@esi.dz>
2025-09-10 18:22:54 +0100

    perceptron

--- Perceptron/Perceptron.py
+++ Perceptron/Perceptron.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class Perceptron:
-  def init(self,lr=0.01, n_iters=1000):
+  def __init__(self,lr=0.01, n_iters=1000):
     self.lr = 0.01
     self.n_iters=1000
     self.activationFunction = self.unitStepFunction
@@ -10,6 +10,19 @@ class Perceptron:
     self.bias = None 
 
   def fit(self,X,y):
+    n_samples, n_features = X.shape
+    #init weights
+    self.weights = np.zeros(n_features)
+    self.bias = 0
+    y_ = np.array([1 if i >0 else 0 for i in y])
+    for _ in range(self.n_iters):
+      for idx, x_i in enumerate(X):
+        linear = np.dot(x_i , self.weights)+self.bias
+        y_predicted = self.activationFunction(linear)
+        update = self.lr*(y_[idx] - y_predicted)
+        self.weights = self.weights + update*x_i
+        self.bias = self.bias + update
+        
     
 
   def predict(self,X):
