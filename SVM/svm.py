b344db66d44d353502537a8a48e56d588eaf47f4
yacineziainia <ny_ziainia@esi.dz>
2025-09-10 19:31:47 +0100

    SVM

--- SVM/svm.py
+++ SVM/svm.py
@@ -1,51 +1,30 @@
 import numpy as np
 
-
-class Perceptron:
-  def __init__(self,lr=0.01, n_iters=1000):
-    self.lr = 0.01
-    self.n_iters=1000
-    self.activationFunction = self.unitStepFunction
-    self.weights = None 
-    self.bias = None 
-
-  def fit(self,X,y):
-    n_samples, n_features = X.shape
-    #init weights
-    self.weights = np.zeros(n_features)
-    self.bias = 0
-    y_ = np.array([1 if i >0 else 0 for i in y])
-    for _ in range(self.n_iters):
-      for idx, x_i in enumerate(X):
-        linear = np.dot(x_i , self.weights)+self.bias
-        y_predicted = self.activationFunction(linear)
-        update = self.lr*(y_[idx] - y_predicted)
-        self.weights = self.weights + update*x_i
-        self.bias = self.bias + update
-        
+#svm
+class SVM:
+    def __init__(self,lr=0.001,lambdaa=0.01,n_iters=1000):
+        self.lr = lr
+        self.lambdaa = lambdaa
+        self.n_iters = n_iters
+        self.w = None
+        self.b = None
     
+    def fit(self,X,y):
+        y_ = np.where(y<=0, -1 , 1)
+        n_samples , n_features = X.shape
 
-  def predict(self,X):
-    linearFunction = np.dot(X,self.weights)+self.bias
-    y_predicted = self.activationFunction(linearFunction)
-    return y_predicted
+        self.w = np.zeros(n_features)
+        self.b = 0
 
+        for _ in range(self.n_iters):
+            for idx,x_i in enumerate(X):
+                cndt = y_[idx] * np.dot(x_i,self.w)+self.b >=1
+                if cndt:
+                    self.w -= self.lr * 2*self.lambdaa*self.w
+                else:
+                    self.w -= self.lr * 2*self.lambdaa*self.w - np.dot(x_i,y_[idx])
+                    self.b -= self.lr * y_[idx]
 
-  def unitStepFunction(self,x):
-    return np.where(x>=0 , 1 ,0 )
-  
-
-#small data set - OR gate
-
-X = np.array([
-    [0, 0],
-    [0, 23],
-    [16, 0]
-])
-y = np.array([0, 1, 1])  
-
-model = Perceptron(lr=0.01, n_iters=10)
-model.fit(X, y)
-X2 = np.array([[100,1]])
-preds = model.predict(X2)
-print("Predictions:", preds)
+    def predict(self,X):
+        linear = np.dot(X, self.w) - self.b
+        return np.sign(linear)
